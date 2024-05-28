9d666b4ce44fbb068a4d35ee2d0617e6cc5b34a5
Yusuf Salih Mamur <93548578+yumamur@users.noreply.github.com>
2024-05-28 21:43:51 +0300

    last (#34)

--- src/parse/is_file_valid.c
+++ src/parse/is_file_valid.c
@@ -1,17 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validate.c                                         :+:      :+:    :+:   */
+/*   is_file_valid.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yumamur <yumamur@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 21:03:10 by yumamur           #+#    #+#             */
-/*   Updated: 2024/04/03 01:26:47 by yumamur          ###   ########.fr       */
+/*   Updated: 2024/05/28 21:35:41 by yumamur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include "libft.h"
+#include <stdio.h>
 
 static _Bool	is_ext_valid(char *filename, char *ext)
 {
@@ -19,7 +20,10 @@ static _Bool	is_ext_valid(char *filename, char *ext)
 
 	file_ext = ft_strrchr(filename, '.');
 	if (file_ext && !ft_strcmp(file_ext, ext))
+	{
+		printf("Invalid file extension\n");
 		return (1);
+	}
 	return (0);
 }
 
@@ -33,6 +37,7 @@ static _Bool	is_file_available(char *filename)
 		close(fd);
 		return (1);
 	}
+	perror("Error\n");
 	return (0);
 }
 
@@ -44,5 +49,6 @@ _Bool	is_file_valid(int argc, char *argv[])
 		&& is_ext_valid(argv[1], ".rt")
 		&& is_file_available(argv[1]))
 		return (1);
+	printf("Invalid file\n");
 	return (0);
 }
